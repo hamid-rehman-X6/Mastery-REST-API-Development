38dfc06b06bb62217aa79a9f12239dd49942c089
Muhammad Hamid Rehman <hamidrehman1012@gmail.com>
2026-02-20 16:30:22 +0500

    implement validation for register api

---
+++ src/middlewares/validationError.ts
@@ -0,0 +1,29 @@
+/**
+ * @copyright 2026 HamidRehman
+ * @license Apache-2.0
+ */
+
+/**
+ * Node Modules
+ */
+import { validationResult } from "express-validator";
+
+/**
+ * Types
+ */
+import { Request, Response, NextFunction } from "express";
+
+const validationError = (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        res.status(400).json({
+            code: 'ValidationError',
+            error: errors.mapped(),
+        });
+        return;
+    }
+    next();
+};
+
+export default validationError;
--- src/routes/v1/auth.ts
+++ src/routes/v1/auth.ts
@@ -18,10 +18,12 @@ import register from '@/controllers/v1/auth/register';
 /**
  * Middlewares
  */
+import validationError from "@/middlewares/validationError";
 
 /**
  * Models
  */
+import User from "@/models/user";
 
 const router = Router();
 
@@ -34,7 +36,25 @@ router.post(
         .isLength({ max: 255 })
         .withMessage('Email must be at most 255 characters long')
         .isEmail()
-        .withMessage('Invalid email format'), 
+        .withMessage('Invalid email format')
+        .custom(async (value) => {
+            const userExists = await User.exists({ email: value });
+            if (userExists) {
+                throw new Error('User email or password is invalid');
+            }
+        }),
+    body('password')
+        .notEmpty()
+        .withMessage('Password is required')
+        .isLength({ min: 8 })
+        .withMessage('Password must be at least 8 characters long'),
+    body('role')
+        .optional()
+        .isString()
+        .withMessage('Role must be a string')
+        .isIn(['user', 'admin'])
+        .withMessage('Role must be either user or admin'),
+    validationError,
     register,
 );
 
