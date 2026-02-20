e4f6bcbb3b6e3ddafb616b90f9507804eb19e5eb
Muhammad Hamid Rehman <hamidrehman1012@gmail.com>
2026-02-20 17:17:46 +0500

    make separate validators for auth

--- src/routes/v1/auth.ts
+++ src/routes/v1/auth.ts
@@ -8,7 +8,11 @@
  * Node Modules
  */
 import { Router } from "express";
-import { body } from 'express-validator';
+
+/**
+ * Custom Modules
+ */
+import { registerValidator } from "@/validators/auth.validator";
 
 /**
  * Controllers
@@ -20,40 +24,11 @@ import register from '@/controllers/v1/auth/register';
  */
 import validationError from "@/middlewares/validationError";
 
-/**
- * Models
- */
-import User from "@/models/user";
-
 const router = Router();
 
 router.post(
     '/register',
-    body('email')
-        .trim()
-        .notEmpty()
-        .withMessage('Email is required')
-        .isLength({ max: 255 })
-        .withMessage('Email must be at most 255 characters long')
-        .isEmail()
-        .withMessage('Invalid email format')
-        .custom(async (value) => {
-            const userExists = await User.exists({ email: value });
-            if (userExists) {
-                throw new Error('User email or password is invalid');
-            }
-        }),
-    body('password')
-        .notEmpty()
-        .withMessage('Password is required')
-        .isLength({ min: 8 })
-        .withMessage('Password must be at least 8 characters long'),
-    body('role')
-        .optional()
-        .isString()
-        .withMessage('Role must be a string')
-        .isIn(['user', 'admin'])
-        .withMessage('Role must be either user or admin'),
+    registerValidator,
     validationError,
     register,
 );
