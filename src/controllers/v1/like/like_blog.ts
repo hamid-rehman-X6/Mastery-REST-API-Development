b5da98ddcfd5fa36a480981692c0cf94ba20f8c9
Muhammad Hamid Rehman <hamidrehman1012@gmail.com>
2026-03-27 23:59:23 +0500

    add like blog controller

--- src/controllers/v1/like/like_blog.ts
+++ src/controllers/v1/like/like_blog.ts
@@ -24,8 +24,37 @@ import Like from '@/models/like';
 import type { Request, Response } from 'express';
 
 const likeBlog = async (req: Request, res: Response): Promise<void> => {
-  const userId = req.userId;
+  const { blogId } = req.params;
+  const { userId } = req.body;
   try {
+    const blog = await Blog.findById(blogId).select('likesCount').exec();
+
+    if (!blog) {
+      res.status(404).json({
+        code: 'NotFound',
+        message: 'Blog not found',
+      });
+      return;
+    }
+
+    const existingLike = await Like.findOne({ blogId, userId }).lean().exec();
+    if (existingLike) {
+      res.status(400).json({
+        code: 'BadRequest',
+        message: 'You have already liked this blog',
+      });
+      return;
+    }
+
+    await Like.create({ blogId, userId });
+    blog.likesCount++;
+    await blog.save();
+
+    logger.info('Blog liked successfully', {
+      userId,
+      blogId: blog._id,
+      likesCount: blog.likesCount,
+    });
   } catch (err) {
     res.status(505).json({
       code: 'ServerError',
